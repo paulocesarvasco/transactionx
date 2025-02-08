3fb94071bcf853fae30a91978843c11919f4e100
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-08 20:59:57 -0300

    Reflect changes at main package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/gorilla/mux"
+	"transactionx/internal/handler"
+	"transactionx/internal/router"
 )
 
 func main() {
@@ -21,13 +20,8 @@ func main() {
 		os.Exit(0)
 	}(c)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/", homePage).Methods("GET")
+	r := router.InstanceRoutes(handler.NewHandler())
 
 	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(map[string]string{"message": "Welcome to the API!"})
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
