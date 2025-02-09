f3f22b5051bca57c3cc3b2305b28702f42f24e5a
paulocesarvasco <paulocvasco.93@gmail.com>
2025-02-09 13:14:16 -0300

    Improve html logic

--- internal/resources/models.go
+++ internal/resources/models.go
@@ -1,7 +1,7 @@
 package resources
 
 type Transaction struct {
-	ID             string  `json:"id,omitempty" gorm:"primaryKey;autoIncrement"`
+	ID             int     `json:"id,omitempty" gorm:"primaryKey;autoIncrement"`
 	Description    string  `json:"description"`
 	Date           string  `json:"transaction_date"`
 	PurchaseAmount float64 `json:"purchase_amount"`
